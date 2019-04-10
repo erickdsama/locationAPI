f05458b20607490baa3e9772bfe7d8a11a79715a
Erick Samaniego <erickdsama@gmail.com>
2019-04-10 15:11:02 -0600

    psycopg2

--- utils/database.py
+++ utils/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///location_rwas.db', convert_unicode=True)
+engine = create_engine(
+    "postgres://qimigqpctbbuom:5ac7371fd5d8d4daf5743c026d3915f986dadfbf9a717b2ba4f1ec95b8d7baea@ec2-23-21-136-232.compute-1.amazonaws.com:5432/d4ndcf7u2mpos1",
+)
+
+# engine = create_engine('sqlite:///location_rwas.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
